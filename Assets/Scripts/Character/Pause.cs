4a4ea8c620357c1ed01834bedc05f70cf020399b
liuglaze <1823892985@qq.com>
2024-09-15 18:40:41 +0800

    暂停

--- Assets/Scripts/Character/Pause.cs
+++ Assets/Scripts/Character/Pause.cs
@@ -4,46 +4,22 @@ using UnityEngine;
 
 public class PauseControl : MonoBehaviour
 {
-    //public bool isPaused = false;
-    //InputActions actions;
-    //private void Awake()
-    //{
-    //    actions = new InputActions();
-    //}
-    //private void OnEnable()
-    //{
-    //    actions.Enable();
-    //    actions.GamePlay.Pause.started += Pause;
-    //}
-    //private void OnDisable()
-    //{
-    //    actions.Disable();
-    //    actions.GamePlay.Pause.started -= Pause;
-    //}
+    public bool isPaused = false;
 
-    //private void Pause(InputAction.CallbackContext context)
-    //{
-    //    if (!isPaused)
-    //    {
-    //        Time.timeScale = 0;
-    //        SwitchToDynamicUpdateMode();
-    //        isPaused = true;
-    //    }
-    //    else
-    //    {
-    //        Time.timeScale = 1f;
-    //        GamePlayUIManager.Instance.ClosePausePanel();
-    //        SwitchToFixedUpdateMode();
-    //        isPaused = false;
-    //    }
-
-    //}
-    //public void SwitchToDynamicUpdateMode()
-    //{
-    //    InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsInDynamicUpdate;
-    //}
-    //public void SwitchToFixedUpdateMode()
-    //{
-    //    InputSystem.settings.updateMode = InputSettings.UpdateMode.ProcessEventsInFixedUpdate;
-    //}
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Time.timeScale = 1f;
+                isPaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0f;
+                isPaused = true;
+            }
+        }
+    }
 }
